b121c1e9f65b161daa1d0721dd087106196643dc
Saturnday <mysaturnday@gmail.com>
2025-08-05 17:25:30 +0800

    First commit develop

--- data/order_data.py
+++
@@ -1,5 +0,0 @@
-class OrderData:
-    valid_ingredients = ["60d3b41abdacab0026a733c6", "609646e4dc916e00276b2870"]
-    invalid_ingredients = ["invalid_hash"]
-    no_ingredients_error = "Ingredient ids must be provided"
-    unauthorized_error = "You should be authorised"
